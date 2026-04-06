a1033148a127cd94b4b283b1efa98ffad3aba252
ErickAndr3s <60296801@nebraska.edu>
2026-04-06 02:08:11 +0000

    Finished Lab 9

--- ImageFilters.py
+++ ImageFilters.py
@@ -1,7 +1,7 @@
 # Lab 9 – Image Processing
-# Name:
-# Date:
-# Assignment:
+# Name:Erick Andres
+# Date:4/5/2026
+# Assignment:Lab 9
 
 from PIL import Image
 
@@ -12,9 +12,14 @@ def swapGreenBlue(img):
     pixels = img.load()
     width, height = img.size
 
+    for x in range(width):
+        for y in range(height):
+            red, green, blue, alpha = pixels[x,y]
+            pixels[x,y] = (red, blue, green, alpha)
+    img.save("swapGB.png")
+
     # TODO: Loop through every pixel and swap green and blue values
 
-    img.save("swapGB.png")
 
 
 def darken(img, amount):
@@ -22,6 +27,17 @@ def darken(img, amount):
     
     pixels = img.load()
     width, height = img.size
+
+    for x in range(width):
+        for y in range(height):
+            red, green, blue, alpha = pixels [x, y]
+
+            red = max(0, red - amount)
+            green = max(0, green - amount)
+            blue = max(0, blue - amount)
+
+            pixels[x, y] = (red, green, blue, alpha)
+
 
     # TODO: Loop through every pixel and reduce RGB values by amount
     # Make sure values do not go below 0
@@ -37,23 +53,23 @@ def bwFilter(img):
 
     for x in range(width):
         for y in range(height):
-            red, green, blue = pixels[x, y]
+            red, green, blue, alpha = pixels[x, y]
             avg = (red + green + blue) // 3
-            pixels[x, y] = (avg, avg, avg)
+            pixels[x, y] = (avg, avg, avg, alpha)
 
-    img.save("bwImg.png")
+    img.save("bwImg.png" )
 
 
 def main():
     # Open the image file
-    myImg = Image.open("durango.png")
+    myImg = Image.open("durango.png").convert("RGBA")
 
     # Example (already completed)
     # bwFilter(myImg)
 
     # Uncomment each function as you complete it
-    # swapGreenBlue(myImg)
-    # darken(myImg, 20)
+    swapGreenBlue(myImg)
+    darken(myImg, 70)
 
 
 if __name__ == "__main__":
